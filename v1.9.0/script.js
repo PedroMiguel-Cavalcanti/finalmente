40867bcb3c09efd7cd99f708e3e64af7162227b5
PedroMiguel-Cavalcanti <131747775+PedroMiguel-Cavalcanti@users.noreply.github.com>
2023-12-05 10:20:25 -0300

    Update script.js

--- v1.9.0/script.js
+++ v1.9.0/script.js
@@ -4,43 +4,103 @@ document.addEventListener('DOMContentLoaded', function () {
 // Define datasets for variations
 var variation1 = {
     data1: {
-        tipo: 'bar',
-        labels: ["Label 1", "Label 2", "Label 3"],
+        tipo: 'pie',
+        labels: ["presença escolar","evasão escolar"],
         datasets: [{
-            label: "Dados 1 - Variação 1",
-            data: [30, 50, 20],
-            backgroundColor: ["red", "green", "blue"],
+            label: "taxa de evasão escolar em porcemtagem total",
+            data: [62,38],
+            backgroundColor: ["rgb(60, 200, 60)", "rgb(255, 120, 120)"],
         }]
     },
     data2:{
         tipo: 'pie',
-        labels: ["A", "B", "C"],
+        labels: ["Escola Municipal", "Escola Estadual", "Escola Federal", "Comunitária", "Confessional", "Filantrópica"],
         datasets: [{
-            label: "Dados 2 - Variação 1",
-            data: [10, 20, 30],
-            backgroundColor: ["orange", "purple", "cyan"],
+            label: "Taxa de Evasão Escolar por tipo escolar",
+            data: [12, 8, 5, 6, 4, 3],
+            backgroundColor: ["#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#0000FF", "#4B0082"],
         }]
+    },
+    data3:{
+        tipo: 'bar',
+        labels: ["Escola Municipal", "Escola Estadual", "Escola Federal", "Comunitária", "Confessional", "Filantrópica"],
+        datasets:[{
+            label: "Afrodescendentes",
+            data: [25,10,18,12,30,22],
+            backgroundColor: ["#FF0000"],
+        },
+        {
+            label: "Caucasiano",
+            data: [20,30,22,18,15,18],
+            backgroundColor: ["#FF7F00"],
+        },
+        {
+            label: "Indígena",
+            data: [15,20,25,25,18,25],
+            backgroundColor: ["#FFFF00"],
+        },
+        {
+            label: "Mestiço",
+            data: [25,15,20,30,22,15],
+            backgroundColor: ["#0000FF"],
+        },
+        {
+            label: "Asiático",
+            data: [15,25,15,15,15,20],
+            backgroundColor: ["#4B0082"],
+        }
+    ]
     }
 };
 
 var variation2 = {
     data1: {
-        tipo: 'bar',
-        labels: ["Label 1", "Label 2", "Label 3"],
+        tipo: 'pie',
+        labels: ["presença escolar","evasão escolar"],
         datasets: [{
-            label: "Dados 1 - Variação 2",
-            data: [50, 10, 20],
-            backgroundColor: ["red", "green", "blue"],
+            label: "taxa de evasão escolar em porcentagem total",
+            data: [45,55],
+            backgroundColor: ["rgb(60, 200, 60)", "rgb(255, 120, 120)"],
         }]
     },
     data2:{
         tipo: 'pie',
-        labels: ["A", "B", "C"],
+        labels: ["Escola Municipal", "Escola Estadual", "Escola Federal", "Comunitária", "Confessional", "Filantrópica"],
         datasets: [{
-            label: "Dados 2 - Variação 2",
-            data: [30, 40, 30],
-            backgroundColor: ["orange", "purple", "cyan"],
+            label: "Taxa de Evasão Escolar por tipo escolar",
+            data: [10, 5, 8, 6, 12, 14],
+            backgroundColor: ["#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#0000FF", "#4B0082"],
         }]
+    },
+    data3:{
+        tipo: 'bar',
+        labels: ["Escola Municipal", "Escola Estadual", "Escola Federal", "Comunitária", "Confessional", "Filantrópica"],
+        datasets:[{
+            label: "Afrodescendentes",
+            data: [15,5,10,20,30,25],
+            backgroundColor: ["#FF0000"],
+        },
+        {
+            label: "Caucasiano",
+            data: [20,15,25,15,20,30],
+            backgroundColor: ["#FF7F00"],
+        },
+        {
+            label: "Indígena",
+            data: [10,25,5,25,10,10],
+            backgroundColor: ["#FFFF00"],
+        },
+        {
+            label: "Mestiço",
+            data: [25,20,30,5,15,15],
+            backgroundColor: ["#0000FF"],
+        },
+        {
+            label: "Asiático",
+            data: [30,35,30,35,25,20],
+            backgroundColor: ["#4B0082"],
+        }
+    ]
     }
 };
 function criarGrafico(ctx, dados, tipo) {
@@ -56,8 +116,10 @@ var selectedVariation = variation1.data1;
 // Create the initial chart using the first variation
 var ctx1 = document.getElementById('canvas1').getContext('2d');
 var ctx2 = document.getElementById('canvas2').getContext('2d');
+var ctx3 = document.getElementById('canvas3').getContext('2d');
 var myChart1 = criarGrafico(ctx1, variation1.data1);
 var myChart2 = criarGrafico(ctx2, variation1.data2);
+var myChart3 = criarGrafico(ctx3, variation1.data3);
 
 // Add event listener to filter change
 filterSelect.addEventListener('change', function () {
@@ -68,14 +130,18 @@ filterSelect.addEventListener('change', function () {
         selectedVariation = variation1.data1;
          myChart1.destroy();
          myChart2.destroy();
+         myChart3.destroy();
          myChart1 = criarGrafico(ctx1, variation1.data1);
          myChart2 = criarGrafico(ctx2, variation1.data2);
+         myChart3 = criarGrafico(ctx2, variation1.data3);
     } else if (selectedFilter === 'filtro2') {
         selectedVariation = variation2.data1;
          myChart1.destroy();
          myChart2.destroy();
+         myChart3.destroy();
          myChart1 = criarGrafico(ctx1, variation2.data1);
          myChart2 = criarGrafico(ctx2, variation2.data2);
+         myChart3 = criarGrafico(ctx3, variation2.data3);
     }
 
     // Update the chart with the new selected variation
